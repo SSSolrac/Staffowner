c47ec9e34b2ff652adfd08670a2b7727cb4f183f
JUAN CARLOS GAMBOL <juancarlos.gambol.cics@ust.edu.ph>
2026-03-29 23:08:11 +0800

    Polish operational clarity and non-technical UX copy

---
+++ src/utils/currency.ts
@@ -0,0 +1,8 @@
+const pesoFormatter = new Intl.NumberFormat('en-PH', {
+  style: 'currency',
+  currency: 'PHP',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+export const formatCurrency = (value: number) => pesoFormatter.format(value);
